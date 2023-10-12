7edf9c3cfebc52bd9e581536a1a7ae52527c9083
4Gatsu <slawek.koczor@o2.pl>
2023-10-12 19:54:39 +0200

    minor tweaks

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/projects.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
