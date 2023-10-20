cfbdd9e83dd0298f2fd773802a1b9901afec6193
4Gatsu <slawek.koczor@o2.pl>
2023-10-20 20:04:29 +0200

    small tweak to colors

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,6 +1,6 @@
 const darkMode = () => {
   const themeToggleBtns = document.querySelectorAll("#theme__toggle");
-
+  // the theme is not saved
   themeToggleBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
       document.body.classList.toggle("light-mode");
